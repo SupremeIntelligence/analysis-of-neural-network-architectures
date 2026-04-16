7c3ead083a3e935cccc193f9b5905155f347c0a0
Supreme Intelligence MacOS <supremeintelligence1602@mail.ru>
2026-04-16 15:01:38 +0300

    Added dataset metadata visualization, improved class mapping, made CNN architecture input-size independent.

--- models/cnn.py
+++ models/cnn.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=6):
+    def __init__(self, in_channels=1, num_classes=6):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1)
+        out_channels = 16
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, 32, kernel_size=3, padding=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(2)
 
-        self.fc1 = nn.Linear(32 * 16 * 16, 128)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc1 = nn.Linear(32, 128) #(B, 32, H/4, W/4)
         self.relu3 = nn.ReLU()
 
         self.fc2 = nn.Linear(128, num_classes)
@@ -29,6 +31,7 @@ class CNN(nn.Module):
         x = self.relu2(x)
         x = self.pool2(x)
 
+        x = self.adaptive_pool(x)
         x = x.view(x.size(0), -1)
 
         x = self.fc1(x)
